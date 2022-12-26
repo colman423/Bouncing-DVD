41b7cc854f3df6b6d1899114ec54273a71f72c02
KaiChu <porkchopwork423@gmail.com>
2022-12-26 14:46:02 +0800

    update

--- index.js
+++ index.js
@@ -1,94 +1,58 @@
-// init prev, next destination
-
-// calculate move time
-// move to next destination
-// listen to next destination
-// calculate next destination base on prev, next destination
-// assign new prev, next destination
-// calculate move time
-// move to next destination
-
 (function () {
-  var MOVE_SPEED = 500;
 
   var containerDom = document.querySelector('#container');
   var dvdDom = document.querySelector('#dvd');
 
-  var cntDest = {};
-  var nextDest = {};
-  var moveVector = { x: 0, y: 0 };
-
   var maxLeft = containerDom.clientWidth - dvdDom.offsetWidth;
   var maxTop = containerDom.clientHeight - dvdDom.offsetHeight;
+  var moveSpeed = ((maxLeft * maxLeft + maxTop * maxTop) ** 0.5) / 2;
+
+  var nextDest = {};
+  var moveVector = { x: 0, y: 0 };
   var transitionedEventCount = 0;
 
-  setTimeout(() => {
+  function init() {
     moveToNextDest(true);
-  }, 1000);
 
-  dvdDom.addEventListener('transitionend', (event) => {
-    console.log('transitionend', event);
-    transitionedEventCount += 1;
-    if (transitionedEventCount === 2) {
-      transitionedEventCount = 0;
-      moveToNextDest();
-    }
-  });
-
-  function logInfo() {
-    console.log('cntDest', cntDest, 'nextDest', nextDest, 'moveVector', moveVector, 'dvdDom.offsetLeft', dvdDom.offsetLeft, 'dvdDom.offsetTop', dvdDom.offsetTop);
+    dvdDom.addEventListener('transitionend', (event) => {
+      transitionedEventCount += 1;
+      if (transitionedEventCount === 2) {
+        // NOTE: wait for transitionedEvents for x & y direction, although they must triggered at the same time
+        transitionedEventCount = 0;
+        moveToNextDest();
+      }
+    });
   }
-
 
   function moveToNextDest(isInit) {
     if (isInit) {
-      // init prev, next destination
-      cntDest = getNormPosition({ top: dvdDom.offsetTop, left: dvdDom.offsetLeft });
-      nextDest = getNormPosition({ top: window.innerHeight / 2, right: 0 });
-      moveVector = getMoveVector();
+      const cntDest = { x: dvdDom.offsetLeft, y: dvdDom.offsetTop };
+      nextDest = { x: maxLeft, y: maxTop / 2 };
+      moveVector = getMoveVector(cntDest, nextDest);
     } else {
-      // calculate next destination base on prev, next destination
-      // assign new prev, next destination
-      cntDest = nextDest;
-      const boundedVector = getBoundedVector();
+      const cntDest = nextDest;
+      const boundedVector = getBoundedVector(moveVector);
       nextDest = getNextDest(cntDest, boundedVector);
-      moveVector = getMoveVector();
+      moveVector = getMoveVector(cntDest, nextDest);
     }
-    logInfo();
 
-    updateDvdStyle();
-    console.log('\n\n\n\n');
+    const moveTime = getMoveTime(moveVector);
+    updateDvdStyle(nextDest.x, nextDest.y, moveTime);
   }
 
-  function getNormPosition(position) {
-    const normPos = {};
 
-    if ('top' in position) {
-      normPos.top = position.top;
-    } else if ('bottom' in position) {
-      normPos.top = maxTop - position.bottom;
+  function getBoundedVector(prevMoveVector) {
+    const isBoundedX = dvdDom.offsetLeft === 0 || dvdDom.offsetLeft === maxLeft;
+    const isBoundedY = dvdDom.offsetTop === 0 || dvdDom.offsetTop === maxTop;
+
+    if (isBoundedX && isBoundedY) {
+      return { x: -prevMoveVector.x, y: -prevMoveVector.y };
+    } else if (isBoundedX) {
+      return { x: -prevMoveVector.x, y: prevMoveVector.y };
+    } else if (isBoundedY) {
+      return { x: prevMoveVector.x, y: -prevMoveVector.y };
     } else {
-      console.error('no top or bottom in position');
-    }
-
-    if ('left' in position) {
-      normPos.left = position.left;
-    } else if ('right' in position) {
-      normPos.left = maxLeft - position.right;
-    } else {
-      console.error('no left or right in position');
-    }
-
-    return normPos;
-  }
-
-  function getBoundedVector() {
-    if (dvdDom.offsetLeft === 0 || dvdDom.offsetLeft === maxLeft) {
-      console.log('x bounded');
-      return { x: -moveVector.x, y: moveVector.y };
-    } else {
-      console.log('y bounded');
-      return { x: moveVector.x, y: -moveVector.y };
+      console.error('not bounded');
     }
   }
 
@@ -96,46 +60,41 @@
     let priorX;
     let priorY;
     if (moveDirection.x > 0) {
-      priorX = (maxLeft - cntDest.left) / moveDirection.x;
+      priorX = (maxLeft - cntDest.x) / moveDirection.x;
     } else {
-      priorX = cntDest.left / -moveDirection.x;
+      priorX = cntDest.x / -moveDirection.x;
     }
 
     if (moveDirection.y > 0) {
-      priorY = (maxTop - cntDest.top) / moveDirection.y;
+      priorY = (maxTop - cntDest.y) / moveDirection.y;
     } else {
-      priorY = cntDest.top / -moveDirection.y;
+      priorY = cntDest.y / -moveDirection.y;
     }
 
     const directionWeight = Math.min(priorX, priorY);
-    console.log(priorX, priorY, directionWeight, cntDest, moveDirection);
     return {
-      left: cntDest.left + moveDirection.x * directionWeight,
-      top: cntDest.top + moveDirection.y * directionWeight,
+      x: cntDest.x + moveDirection.x * directionWeight,
+      y: cntDest.y + moveDirection.y * directionWeight,
     };
   }
 
-  function getMoveVector() {
-    moveVector = {
-      x: nextDest.left - cntDest.left,
-      y: nextDest.top - cntDest.top,
+  function getMoveVector(cntDest, nextDest) {
+    return {
+      x: nextDest.x - cntDest.x,
+      y: nextDest.y - cntDest.y,
     };
-    return moveVector;
   }
 
-  function getMoveTime() {
-    const displacement = moveVector.x * moveVector.x + moveVector.y * moveVector.y;
-    return Math.sqrt(displacement) / MOVE_SPEED;
+  function getMoveTime(moveVector) {
+    const displacement = Math.sqrt(moveVector.x * moveVector.x + moveVector.y * moveVector.y);
+    return displacement / moveSpeed;
   }
 
-  function updateDvdStyle() {
-    const moveTime = getMoveTime();
-    console.log('moveTime', moveTime);
-
+  function updateDvdStyle(x, y, moveTime) {
     dvdDom.style.transitionDuration = `${moveTime}s`;
-    Object.entries(nextDest).forEach(([k, v]) => {
-      dvdDom.style[k] = `${v}px`;
-    });
+    dvdDom.style.left = `${x}px`;
+    dvdDom.style.top = `${y}px`;
   }
 
+  setTimeout(init, 1000);
 })();
